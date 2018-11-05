a961d356d71ff6a4a894b444be4c267c828af4fc
FlorenceSoudain <43751399+FlorenceSoudain@users.noreply.github.com>
2018-11-05 12:20:13 +0100

    V2

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-var nombreAleatoire = Math.random()*100;
-nombreAleatoire = Math.round(nombreAleatoire);
-
-var essais = 1;
+var essais = 9;
 
 var choixUtilisateur = 0;
+
+var nombreAleatoire = Math.random()*100;
+nombreAleatoire = Math.round(nombreAleatoire);
 
 document.getElementById('envoie').addEventListener('click', function () {
 
     choixUtilisateur = document.getElementById('proposition').value;
+    choixOrdi = nombreAleatoire;
+    essais = essais--;
 
-    if (nombreAleatoire < choixUtilisateur)
+    if (choixOrdi < choixUtilisateur)
     {
-        alert('Le chiffre choisi est trop grand');
-        essais = essais + 1;
-        choixUtilisateur = document.getElementById('proposition').value;
+        document.getElementById('rep').innerHTML = "Le chiffre choisi est trop grand.";
+        document.getElementById('reste').innerHTML = "Il vous reste " + essais-- + " essais.";
     }
-    if (nombreAleatoire > choixUtilisateur)
+    if (choixOrdi > choixUtilisateur)
     {
-        alert('Le chiffre choisi est trop petit');
-        essais = essais + 1;
-        choixUtilisateur = document.getElementById('proposition').value;
+        document.getElementById('rep').innerHTML = "Le chiffre choisi est trop petit.";
+        document.getElementById('reste').innerHTML = "Il vous reste " + essais-- + " essais.";
     }
-    if (nombreAleatoire === choixUtilisateur)
+    if (choixOrdi == choixUtilisateur)
     {
-        alert('gagné')
+        document.getElementById('rep').innerHTML = "Félicitation. Vous avez gagné !";
+        document.getElementById('rep').style.color = "red";
+        document.getElementById('rep').style.border = "solid";
+        document.getElementById('rep').style.background = "pink";
+        document.getElementById('rep').style.display = "inline";
+        document.getElementById('rep').style.padding = "10px";
+        document.getElementById('reste').style.display = "none"
     }
-    if (essais === 10)
+    if (essais < 0)
     {
-        alert('fin')
+        document.getElementById('reste').innerHTML = "Vous n'avez plus d'essais. Vous avez perdu.";
+        document.getElementById('envoie').style.display = "none";
     }
+});
+
+document.getElementById('rejouer').addEventListener('click', function() {
+
+    nombreAleatoire = Math.random()*100;
+    nombreAleatoire = Math.round(nombreAleatoire);
+    essais = 10;
+    choixUtilisateur = 0;
+    document.getElementById('envoie').style.display = "inline";
+    document.getElementById('rep').innerHTML = "Nouvel essai.";
+    document.getElementById('reste').innerHTML = " Bonne chance.";
+    document.getElementById('rep').style.color = "black";
+    document.getElementById('rep').style.border = "none";
+    document.getElementById('rep').style.background = "none";
+    document.getElementById('reste').style.display = "block";
+    document.getElementById('reste').style.marginTop = "15px"
 });
